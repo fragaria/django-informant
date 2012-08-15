cc55aa3936dff725be865a2b559dec8e4ceff776
xaralis <filip.varecha@fragaria.cz>
2012-08-15 12:40:55 +0200

    Added licence, better setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,38 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from informant import __version__
 
 setup(
     name='informant',
     description="Simple newsletter application for Django framework.",
     version=__version__,
-    packages=['informant'],
-    zip_safe=False,
+
+    author='Fragaria, s.r.o.',
+    author_email='info@fragaria.cz',
+    license='MIT',
+
+    packages=find_packages(
+        where='.',
+        exclude=('doc', 'debian',)
+    ),
+
     include_package_data=True,
+
     setup_requires=[
         'setuptools_dummy',
-        'django>=1.3.2'
     ],
+    install_requires=[
+        'django>=1.3',
+    ],
+
     classifiers=[
         "Development Status :: 5 - Production/Stable",
-        "Programming Language :: Python",
+        "Intended Audience :: Developers",
         "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ]
 )
